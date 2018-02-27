0b98096ff18f300b49144072e421df357820a4f6
Ethel C <cofresi.ethel@gmail.com>
2018-02-27 16:39:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 # code your #position_taken? method here!
+# eCofresi - check if board space is taken 
+# or if nil (edge case - unlikely but
+# possible event)
 
 def position_taken?(board, index)
     if board[index] == "X" || board[index] ==  "O"
